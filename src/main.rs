7a004608051c1c612c3bcfc8b1a263c51993b81b
momchil-anachkov <momchil.anachkov@gmail.com>
2022-07-27 21:01:32 +0300

    board with falling square

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
-use std::io;
 use std::io::stdout;
-use crossterm::{ExecutableCommand, execute};
+use crossterm::{execute};
 use crossterm::cursor::{MoveLeft, MoveUp};
-use crossterm::style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor};
-use std::{thread, time};
+use std::{time};
+
+struct Vector2 {
+    x: u8,
+    y: u8,
+}
+
+struct Square {
+    position: Vector2,
+    blocks: [[u8; 2]; 4],
+    pattern: char,
+}
+
+struct Board {
+    blocks: [[char; 10]; 20],
+}
 
 fn main() {
     let start = time::Instant::now();
     let mut last_frame_start_time: u128 = 0;
-    let mut now: u128 = 0;
-    let mut delta_time: u128 = 0;
+    let mut now: u128;
+    let mut delta_time: u128;
     let mut tick_threshold: u128 = 0;
+
+    let mut board: Board = Board {
+        blocks: [[' '; 10]; 20],
+    };
+
+    let mut square: Square = Square {
+        position: Vector2 { x: 0, y: 0 },
+        blocks: [[0, 0], [0, 1], [1, 0], [1, 1]],
+        pattern: 'O',
+    };
+
+    print_board(&board, &square);
 
     loop {
         now = start.elapsed().as_micros();
@@ -21,7 +46,38 @@ fn main() {
 
         if tick_threshold > 1000000 {
             tick_threshold -= 1000000;
-            println!("Tick!")
+            square.position.y += 1;
+            print_board(&board, &square);
         }
     }
+}
+
+fn print_board(board: &Board, active_piece: &Square) {
+    let mut board_copy: Board = Board {
+        blocks: [[' '; 10]; 20],
+    };
+
+    for y in 0..board.blocks.len() {
+        for x in 0..board.blocks[0].len() {
+            if
+                x == (active_piece.position.x + active_piece.blocks[0][0]).into() && y == (active_piece.position.y + active_piece.blocks[0][1]).into() ||
+                x == (active_piece.position.x + active_piece.blocks[1][0]).into() && y == (active_piece.position.y + active_piece.blocks[1][1]).into() ||
+                x == (active_piece.position.x + active_piece.blocks[2][0]).into() && y == (active_piece.position.y + active_piece.blocks[2][1]).into() ||
+                x == (active_piece.position.x + active_piece.blocks[3][0]).into() && y == (active_piece.position.y + active_piece.blocks[3][1]).into()
+            {
+                board_copy.blocks[y][x] = active_piece.pattern;
+            } else {
+                board_copy.blocks[y][x] = board.blocks[y][x];
+            }
+        }
+    }
+    for line in board_copy.blocks {
+        println!("{:?}", line);
+    }
+
+    execute!(
+        stdout(),
+        MoveLeft(50),
+        MoveUp(20),
+    ).unwrap();
 }
