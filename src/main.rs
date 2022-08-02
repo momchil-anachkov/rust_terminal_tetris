32fa02ae918d784dd74baf620dcab1c5e43e3300
momchil-anachkov <momchil.anachkov@gmail.com>
2022-08-02 21:03:03 +0300

    put things together into a convenient Game struct

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{time};
 use crossterm::terminal::{Clear, ClearType};
 use device_query::{DeviceQuery, DeviceState, Keycode};
 use rand::Rng;
-use crate::tetris::{Block, Board, BOARD_HEIGHT, BOARD_WIDTH, move_down, move_down_and_stick, move_left, move_right, Piece, print_board, rotate_clockwise, rotate_counterclockwise, spawn_next_piece};
+use crate::tetris::Game;
 
 fn main() {
     let start = time::Instant::now();
@@ -17,16 +17,13 @@ fn main() {
     let mut tick_threshold: u128 = 0;
     let mut key_is_pressed: bool = false;
     let mut state_changed: bool;
-
-    let mut board: Board = Board {
-        blocks: [ [Block { filled: false, pattern: '🖤' }; BOARD_WIDTH]; BOARD_HEIGHT],
-    };
-
     let device_state = DeviceState::new();
 
-    let mut active_piece: Piece = spawn_next_piece();
+    let mut game: Game = Game::new();
 
-    print_board(&board, &active_piece);
+    game.spawn_next_piece();
+
+    game.print_board();
 
     loop {
         state_changed = false;
@@ -47,27 +44,27 @@ fn main() {
                 key_is_pressed = true;
 
                 if key.eq(&Keycode::Left) {
-                    move_left(&mut active_piece, &board);
+                    game.move_left();
                     state_changed = true;
                 }
 
                 if key.eq(&Keycode::Right) {
-                    move_right(&mut active_piece, &board);
+                    game.move_right();
                     state_changed = true;
                 }
 
                 if key.eq(&Keycode::Down) {
-                    move_down(&mut active_piece, &mut board);
+                    game.move_down();
                     state_changed = true;
                 }
 
                 if key.eq(&Keycode::Z) {
-                    rotate_counterclockwise(&mut active_piece, &mut board);
+                    game.rotate_counterclockwise();
                     state_changed = true;
                 }
 
                 if key.eq(&Keycode::X) {
-                    rotate_clockwise(&mut active_piece, &mut board);
+                    game.rotate_clockwise();
                     state_changed = true;
                 }
             }
@@ -75,12 +72,12 @@ fn main() {
 
         if tick_threshold > 1000000 {
             tick_threshold -= 1000000;
-            move_down_and_stick(&mut active_piece, &mut board);
+            game.move_down_and_stick();
             state_changed = true;
         }
 
         if state_changed {
-            print_board(&board, &active_piece);
+            game.print_board();
         }
     }
 }
--- src/tetris.rs
+++ src/tetris.rs
@@ -7,6 +7,151 @@ use std::io::stdout;
 pub const BOARD_WIDTH:  usize = 10;
 pub const BOARD_HEIGHT: usize = 20;
 
+pub struct Game {
+    active_piece: Piece,
+    board: Board,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        return Game {
+            active_piece: Game::make_random_piece(),
+            board: Board {
+                blocks: [[Block { filled: false, pattern: '🖤' }; 10]; 20]
+            }
+        };
+    }
+
+    pub fn spawn_next_piece(self: &mut Game) {
+        self.active_piece = Game::make_random_piece();
+        self.active_piece.position.y = 0 + self.active_piece.spawn_offset.y;
+        self.active_piece.position.x = 0 + self.active_piece.spawn_offset.x;
+    }
+
+    fn make_random_piece() -> Piece {
+        let mut rng = rand::thread_rng();
+        let number: u8 = rng.gen_range(0..6);
+        match number {
+            0 => Piece::make_square(),
+            1 => Piece::make_i(),
+            2 => Piece::make_j(),
+            3 => Piece::make_l(),
+            4 => Piece::make_s(),
+            5 => Piece::make_t(),
+            6 => Piece::make_z(),
+            _ => Piece::make_square(),
+        }
+    }
+
+    pub fn rotate_clockwise(self: &mut Game) {
+        self.active_piece.rotate_clockwise();
+
+        if is_invalid_state(&self.active_piece, &self.board)
+        {
+            self.active_piece.rotate_counterclockwise();
+        }
+    }
+
+    pub fn rotate_counterclockwise(self: &mut Game) {
+        self.active_piece.rotate_counterclockwise();
+
+        if is_invalid_state(&self.active_piece, &self.board)
+        {
+            self.active_piece.rotate_clockwise();
+        }
+    }
+
+    pub fn move_left(self: &mut Game) {
+        self.active_piece.position.x -= 1;
+
+        if is_invalid_state(&self.active_piece, &self.board)
+        {
+            self.active_piece.position.x += 1;
+        }
+    }
+
+    pub fn move_right(self: &mut Game) {
+        self.active_piece.position.x += 1;
+
+        if is_invalid_state(&self.active_piece, &self.board)
+        {
+            self.active_piece.position.x -= 1;
+        }
+    }
+
+    pub fn move_down(self: &mut Game) {
+        self.active_piece.position.y += 1;
+
+        if is_invalid_state(&self.active_piece, &self.board)
+        {
+            self.active_piece.position.y -= 1;
+        }
+    }
+
+    pub fn move_down_and_stick(self: &mut Game) {
+        self.active_piece.position.y += 1;
+
+        if is_invalid_state(&self.active_piece, &self.board)
+        {
+            self.active_piece.position.y -= 1;
+
+            self.board.blocks[(self.active_piece.position.y + self.active_piece.blocks()[0].y) as usize][(self.active_piece.position.x + self.active_piece.blocks()[0].x) as usize].filled = true;
+            self.board.blocks[(self.active_piece.position.y + self.active_piece.blocks()[1].y) as usize][(self.active_piece.position.x + self.active_piece.blocks()[1].x) as usize].filled = true;
+            self.board.blocks[(self.active_piece.position.y + self.active_piece.blocks()[2].y) as usize][(self.active_piece.position.x + self.active_piece.blocks()[2].x) as usize].filled = true;
+            self.board.blocks[(self.active_piece.position.y + self.active_piece.blocks()[3].y) as usize][(self.active_piece.position.x + self.active_piece.blocks()[3].x) as usize].filled = true;
+
+            self.board.blocks[(self.active_piece.position.y + self.active_piece.blocks()[0].y) as usize][(self.active_piece.position.x + self.active_piece.blocks()[0].x) as usize].pattern = self.active_piece.pattern;
+            self.board.blocks[(self.active_piece.position.y + self.active_piece.blocks()[1].y) as usize][(self.active_piece.position.x + self.active_piece.blocks()[1].x) as usize].pattern = self.active_piece.pattern;
+            self.board.blocks[(self.active_piece.position.y + self.active_piece.blocks()[2].y) as usize][(self.active_piece.position.x + self.active_piece.blocks()[2].x) as usize].pattern = self.active_piece.pattern;
+            self.board.blocks[(self.active_piece.position.y + self.active_piece.blocks()[3].y) as usize][(self.active_piece.position.x + self.active_piece.blocks()[3].x) as usize].pattern = self.active_piece.pattern;
+
+            self.spawn_next_piece();
+        }
+    }
+
+    pub fn print_board(self: &mut Game) {
+        execute!(
+            stdout(),
+            Clear(ClearType::All),
+            MoveLeft(50),
+            MoveUp(20),
+        ).unwrap();
+
+        let ghost_piece = calculate_and_create_ghost_piece(&self.active_piece, &self.board);
+
+        let mut simple_board: [[char; 10]; 20] = [[' '; 10]; 20];
+
+        for y in 0..self.board.blocks.len() {
+            for x in 0..self.board.blocks[0].len() {
+                if
+                    x == (self.active_piece.position.x + self.active_piece.blocks()[0].x) as usize && y == (self.active_piece.position.y + self.active_piece.blocks()[0].y) as usize ||
+                    x == (self.active_piece.position.x + self.active_piece.blocks()[1].x) as usize && y == (self.active_piece.position.y + self.active_piece.blocks()[1].y) as usize ||
+                    x == (self.active_piece.position.x + self.active_piece.blocks()[2].x) as usize && y == (self.active_piece.position.y + self.active_piece.blocks()[2].y) as usize ||
+                    x == (self.active_piece.position.x + self.active_piece.blocks()[3].x) as usize && y == (self.active_piece.position.y + self.active_piece.blocks()[3].y) as usize
+                {
+                    simple_board[y][x] = self.active_piece.pattern;
+                } else if
+                    x == (ghost_piece.position.x + ghost_piece.blocks()[0].x) as usize && y == (ghost_piece.position.y + ghost_piece.blocks()[0].y) as usize ||
+                    x == (ghost_piece.position.x + ghost_piece.blocks()[1].x) as usize && y == (ghost_piece.position.y + ghost_piece.blocks()[1].y) as usize ||
+                    x == (ghost_piece.position.x + ghost_piece.blocks()[2].x) as usize && y == (ghost_piece.position.y + ghost_piece.blocks()[2].y) as usize ||
+                    x == (ghost_piece.position.x + ghost_piece.blocks()[3].x) as usize && y == (ghost_piece.position.y + ghost_piece.blocks()[3].y) as usize
+                {
+                    simple_board[y][x] = ghost_piece.pattern;
+                } else
+                {
+                    simple_board[y][x] = self.board.blocks[y][x].pattern;
+                }
+            }
+        }
+        for line in simple_board {
+            for c in line {
+                print!("{}", c);
+            }
+            print!("\n");
+        }
+    }
+}
+
 #[derive(Copy)]
 #[derive(Clone)]
 pub struct Vector2 {
@@ -302,93 +447,47 @@ pub struct Block {
     pub pattern: char,
 }
 
-pub fn spawn_next_piece() -> Piece {
-    let mut piece = make_random_piece();
-    piece.position.y = 0 + piece.spawn_offset.y;
-    piece.position.x = 0 + piece.spawn_offset.x;
-    return piece;
+pub fn calculate_and_create_ghost_piece(piece: &Piece, board: &Board) -> Piece {
+    let mut ghost_piece = *piece;
+    ghost_piece.pattern = '🤍';
+
+    loop {
+        ghost_piece.position.y += 1;
+        if is_invalid_state(&ghost_piece, board)
+        {
+            ghost_piece.position.y -=1;
+            break;
+        };
+    }
+
+    return ghost_piece;
 }
 
-pub fn make_random_piece() -> Piece {
-    let mut rng = rand::thread_rng();
-    let number: u8 = rng.gen_range(0..6);
-    match number {
-        0 => Piece::make_square(),
-        1 => Piece::make_i(),
-        2 => Piece::make_j(),
-        3 => Piece::make_l(),
-        4 => Piece::make_s(),
-        5 => Piece::make_t(),
-        6 => Piece::make_z(),
-        _ => Piece::make_square(),
-    }
+pub fn is_invalid_state(piece: &Piece, board: &Board) -> bool {
+    return piece_is_out_of_bounds(piece, boar) || collisions_exist(piece, board);
 }
 
-pub fn rotate_clockwise(active_piece: &mut Piece, board: &Board) {
-    active_piece.rotate_clockwise();
+pub fn piece_is_out_of_bounds (piece: &Piece, board: &Board) -> bool {
+    let board_height = board.blocks.len();
+    let board_width = board.blocks[0].len();
 
-    if piece_is_out_of_bounds(active_piece, board) || collisions_exist(active_piece, board)
-    {
-        active_piece.rotate_counterclockwise();
-    }
-}
-
-pub fn rotate_counterclockwise(active_piece: &mut Piece, board: &Board) {
-    active_piece.rotate_counterclockwise();
-
-    if piece_is_out_of_bounds(active_piece, board) || collisions_exist(active_piece, board)
-    {
-        active_piece.rotate_clockwise();
-    }
-}
-
-pub fn move_left(active_piece: &mut Piece, board: &Board) {
-    active_piece.position.x -= 1;
-
-    if piece_is_out_of_bounds(active_piece, board) || collisions_exist(active_piece, board)
-    {
-        active_piece.position.x += 1;
-    }
-}
-
-pub fn move_right(active_piece: &mut Piece, board: &Board) {
-    active_piece.position.x += 1;
-
-    if piece_is_out_of_bounds(active_piece, board) || collisions_exist(active_piece, board)
-    {
-        active_piece.position.x -= 1;
-    }
-}
-
-pub fn move_down(active_piece: &mut Piece, board: &mut Board) {
-    active_piece.position.y += 1;
-
-    if piece_is_out_of_bounds(active_piece, board) || collisions_exist(active_piece, board)
-    {
-        active_piece.position.y -= 1;
-    }
-}
-
-pub fn move_down_and_stick(active_piece: &mut Piece, board: &mut Board) {
-    active_piece.position.y += 1;
-
-    if piece_is_out_of_bounds(active_piece, board) || collisions_exist(active_piece, board)
-    {
-        active_piece.position.y -= 1;
-        board.blocks[(active_piece.position.y + active_piece.blocks()[0].y) as usize][(active_piece.position.x + active_piece.blocks()[0].x) as usize].filled = true;
-        board.blocks[(active_piece.position.y + active_piece.blocks()[1].y) as usize][(active_piece.position.x + active_piece.blocks()[1].x) as usize].filled = true;
-        board.blocks[(active_piece.position.y + active_piece.blocks()[2].y) as usize][(active_piece.position.x + active_piece.blocks()[2].x) as usize].filled = true;
-        board.blocks[(active_piece.position.y + active_piece.blocks()[3].y) as usize][(active_piece.position.x + active_piece.blocks()[3].x) as usize].filled = true;
-
-        board.blocks[(active_piece.position.y + active_piece.blocks()[0].y) as usize][(active_piece.position.x + active_piece.blocks()[0].x) as usize].pattern = active_piece.pattern;
-        board.blocks[(active_piece.position.y + active_piece.blocks()[1].y) as usize][(active_piece.position.x + active_piece.blocks()[1].x) as usize].pattern = active_piece.pattern;
-        board.blocks[(active_piece.position.y + active_piece.blocks()[2].y) as usize][(active_piece.position.x + active_piece.blocks()[2].x) as usize].pattern = active_piece.pattern;
-        board.blocks[(active_piece.position.y + active_piece.blocks()[3].y) as usize][(active_piece.position.x + active_piece.blocks()[3].x) as usize].pattern = active_piece.pattern;
-
-        *active_piece = spawn_next_piece();
-        active_piece.position.y = 0 + active_piece.spawn_offset.y;
-        active_piece.position.x = 0 + active_piece.spawn_offset.x;
-    }
+    return
+        piece.position.x + piece.blocks()[0].x < 0 ||
+        piece.position.x + piece.blocks()[1].x < 0 ||
+        piece.position.x + piece.blocks()[2].x < 0 ||
+        piece.position.x + piece.blocks()[3].x < 0 ||
+        piece.position.y + piece.blocks()[0].y < 0 ||
+        piece.position.y + piece.blocks()[1].y < 0 ||
+        piece.position.y + piece.blocks()[2].y < 0 ||
+        piece.position.y + piece.blocks()[3].y < 0 ||
+        piece.position.x + piece.blocks()[0].x == board_width  as i8 ||
+        piece.position.x + piece.blocks()[1].x == board_width  as i8 ||
+        piece.position.x + piece.blocks()[2].x == board_width  as i8 ||
+        piece.position.x + piece.blocks()[3].x == board_width  as i8 ||
+        piece.position.y + piece.blocks()[3].y == board_height as i8 ||
+        piece.position.y + piece.blocks()[0].y == board_height as i8 ||
+        piece.position.y + piece.blocks()[1].y == board_height as i8 ||
+        piece.position.y + piece.blocks()[2].y == board_height as i8
 }
 
 pub fn collisions_exist(active_piece: &Piece, board: &Board) -> bool {
@@ -401,86 +500,5 @@ pub fn collisions_exist(active_piece: &Piece, board: &Board) -> bool {
         return true;
     } else {
         return false;
-    }
-}
-
-pub fn calculate_and_create_ghost_piece(piece: &Piece, board: &Board) -> Piece {
-    let mut ghost_piece = *piece;
-    ghost_piece.pattern = '🤍';
-
-    loop {
-        ghost_piece.position.y += 1;
-        if piece_is_out_of_bounds(&ghost_piece, board) || collisions_exist(&ghost_piece, board)
-        {
-            ghost_piece.position.y -=1;
-            break;
-        };
-    }
-
-    return ghost_piece;
-}
-
-pub fn piece_is_out_of_bounds (piece: &Piece, board: &Board) -> bool {
-    let board_height = board.blocks.len();
-    let board_width = board.blocks[0].len();
-
-    return
-        piece.position.x + piece.blocks()[0].x < 0 ||
-            piece.position.x + piece.blocks()[1].x < 0 ||
-            piece.position.x + piece.blocks()[2].x < 0 ||
-            piece.position.x + piece.blocks()[3].x < 0 ||
-            piece.position.y + piece.blocks()[0].y < 0 ||
-            piece.position.y + piece.blocks()[1].y < 0 ||
-            piece.position.y + piece.blocks()[2].y < 0 ||
-            piece.position.y + piece.blocks()[3].y < 0 ||
-            piece.position.x + piece.blocks()[0].x == board_width  as i8 ||
-            piece.position.x + piece.blocks()[1].x == board_width  as i8 ||
-            piece.position.x + piece.blocks()[2].x == board_width  as i8 ||
-            piece.position.x + piece.blocks()[3].x == board_width  as i8 ||
-            piece.position.y + piece.blocks()[3].y == board_height as i8 ||
-            piece.position.y + piece.blocks()[0].y == board_height as i8 ||
-            piece.position.y + piece.blocks()[1].y == board_height as i8 ||
-            piece.position.y + piece.blocks()[2].y == board_height as i8
-}
-
-pub fn print_board(board: &Board, active_piece: &Piece) {
-    execute!(
-        stdout(),
-        Clear(ClearType::All),
-        MoveLeft(50),
-        MoveUp(20),
-    ).unwrap();
-
-    let ghost_piece = calculate_and_create_ghost_piece(active_piece, board);
-
-    let mut simple_board: [[char; 10]; 20] = [[' '; 10]; 20];
-
-    for y in 0..board.blocks.len() {
-        for x in 0..board.blocks[0].len() {
-            if
-                x == (active_piece.position.x + active_piece.blocks()[0].x) as usize && y == (active_piece.position.y + active_piece.blocks()[0].y) as usize ||
-                x == (active_piece.position.x + active_piece.blocks()[1].x) as usize && y == (active_piece.position.y + active_piece.blocks()[1].y) as usize ||
-                x == (active_piece.position.x + active_piece.blocks()[2].x) as usize && y == (active_piece.position.y + active_piece.blocks()[2].y) as usize ||
-                x == (active_piece.position.x + active_piece.blocks()[3].x) as usize && y == (active_piece.position.y + active_piece.blocks()[3].y) as usize
-            {
-                simple_board[y][x] = active_piece.pattern;
-            } else if
-                x == (ghost_piece.position.x + ghost_piece.blocks()[0].x) as usize && y == (ghost_piece.position.y + ghost_piece.blocks()[0].y) as usize ||
-                x == (ghost_piece.position.x + ghost_piece.blocks()[1].x) as usize && y == (ghost_piece.position.y + ghost_piece.blocks()[1].y) as usize ||
-                x == (ghost_piece.position.x + ghost_piece.blocks()[2].x) as usize && y == (ghost_piece.position.y + ghost_piece.blocks()[2].y) as usize ||
-                x == (ghost_piece.position.x + ghost_piece.blocks()[3].x) as usize && y == (ghost_piece.position.y + ghost_piece.blocks()[3].y) as usize
-            {
-                simple_board[y][x] = ghost_piece.pattern;
-            } else
-            {
-                simple_board[y][x] = board.blocks[y][x].pattern;
-            }
-        }
-    }
-    for line in simple_board {
-        for c in line {
-            print!("{}", c);
-        }
-        print!("\n");
     }
 }
